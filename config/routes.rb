857473a77580b4eddf2a999b20520012c2b5bab5
Mark Prospero <mark_lee31@hotmail.com>
2021-03-29 23:52:10 +0200

    portfolio tomando jeito

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/home', to: 'pages#home', as: :home
+  get '/projects', to: 'pages#projects', as: :projects
+  get '/aboutme', to: 'pages#aboutme', as: :aboutme
 end
